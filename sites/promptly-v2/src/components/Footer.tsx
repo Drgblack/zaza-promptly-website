0617bc151dcca6480cf62c8ba2c8dec89bae9605
Dr. Greg Blackburn <greg@zazatechnologies.com>
2025-08-24 10:46:35 +0200

    Update Footer.tsx

--- sites/promptly-v2/src/components/Footer.tsx
+++ sites/promptly-v2/src/components/Footer.tsx
@@ -139,7 +139,10 @@ export default function Footer() {
                 </svg>
                 Student Privacy Protected
               </div>
-              
+              <span id="build-marker" className="ml-2 text-xs opacity-60">
+  build 2025-08-24 10:50 UTC
+</span>
+
               <div className="flex items-center text-sm text-gray-500 dark:text-gray-500">
                 <svg className="w-4 h-4 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
